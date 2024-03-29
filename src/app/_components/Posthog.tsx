d0247802842d84fb8f5c76360296a534699f62dd
Rob Gordon <rob.gordon@tone-row.com>
2024-03-29 11:43:56 -0400

    Add posthog

---
+++ src/app/_components/Posthog.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { PostHogProvider, usePostHog } from "posthog-js/react";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+};
+
+export function Posthog({ children }: { children: React.ReactNode }) {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
+    return (
+      <PostHogProvider
+        apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+        options={options}
+      >
+        {children}
+        <TrackPageViews />
+      </PostHogProvider>
+    );
+  }
+  return <>{children}</>;
+}
+
+export function TrackPageViews() {
+  const posthog = usePostHog();
+
+  const location = usePathname();
+
+  useEffect(() => {
+    if (!posthog) return;
+
+    // new
+    posthog.capture("$pageview");
+  }, [location, posthog]);
+
+  return null;
+}
